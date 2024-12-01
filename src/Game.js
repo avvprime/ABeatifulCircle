4879a511b2026acf20a926c49ac2abf9d76be65e
avvprime <75721379+avvprime@users.noreply.github.com>
2024-12-02 02:05:32 +0300

    Restart with light version of game

--- src/Game.js
+++ src/Game.js
@@ -1,37 +1,6 @@
-
-import GameInitState from "./game_states/GameInitState";
-
 export default class Game{
-
-    States = {
-        INIT: new GameInitState(this, "Game Init State"),
-    };
-
-    
-    Textures = {
-        /*
-        key: {
-            width: xx,
-            height: xx
-        }
-        */
-    };
-
-    state = undefined;
-
     constructor()
     {
-        this.state = this.States.INIT;
-        this.state.enter();
+        
     }
-
-    changeState(newState)
-    {
-        this.state.exit();
-        this.state = newState;
-        this.state.enter();
-    }
-
-
-
 }
